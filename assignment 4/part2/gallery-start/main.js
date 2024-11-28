54cebae0e51d04d6f5ac45a2134184326af9ddc6
Alexander-Lekawski <alexander.lekawski@dcmail.ca>
2024-11-28 13:53:29 -0500

    a4 part 2 done

--- assignment 4/part2/gallery-start/main.js
+++ assignment 4/part2/gallery-start/main.js
@@ -5,14 +5,34 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const filenames= ["images/pic1.jpg", "images/pic2.jpg", "images/pic3.jpg", "images/pic4.jpg", "images/pic5.jpg"]
 
 /* Declaring the alternative text for each image file */
-
+const alternative = ["Closeup of a human eye", "Rock with a wave design", "Purple and white flowers", "Wall in a tomb", "Moth on a leaf"]
 /* Looping through images */
+const displayedImg = document.querySelector('.displayed-img');
+for (let i = 0; i < 5; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', filenames[i]);
+    newImage.setAttribute('alt', alternative[i]);
 
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
-
+    newImage.addEventListener('click', function () {
+        displayedImg.setAttribute('src', newImage.getAttribute('src')); // Set displayed image src
+        displayedImg.setAttribute('alt', newImage.getAttribute('alt'));
+    });
+    thumbBar.appendChild(newImage);
+}
 /* Wiring up the Darken/Lighten button */
+btn.addEventListener('click', function () {
+    const currentClass = btn.getAttribute('class'); // Get current class name
+
+    if (currentClass === 'dark') {
+        btn.setAttribute('class', 'light'); // Change class to 'light'
+        btn.textContent = 'Lighten';        // Update button text
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 50%)'; // Dark overlay
+    } else {
+        btn.setAttribute('class', 'dark');  // Change class to 'dark'
+        btn.textContent = 'Darken';         // Update button text
+        overlay.style.backgroundColor = 'rgb(0 0 0 / 0%)'; // Remove overlay
+    }
+});
